1e6fa7e88a3778eba478e4b02b85bd9b5d778f18
Geromet <gerome.tokic@gmail.com>
2021-07-21 15:04:43 +0200

    Fixed multi tapping + maybe bottom info text.

--- Assets/Scripts/Clicking.cs
+++ Assets/Scripts/Clicking.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-
-public class Clicking : MonoBehaviour, IPointerClickHandler
+public class Clicking : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    public void OnPointerClick(PointerEventData eventData)
+    bool clicked = false;
+    public void OnPointerDown(PointerEventData eventData)
     {
-        Controller.instance.GenerateDollars();
+        if (clicked == false)
+        {
+            Controller.instance.GenerateDollars();
+            clicked = true;
+        }
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        clicked = false;
     }
 }
